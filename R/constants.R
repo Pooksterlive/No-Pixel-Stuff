b71130a3a2ce4e0b47c58d1a516ffe68e1455e4d
Jackson Joyner <83993101+Pooksterlive@users.noreply.github.com>
2026-09-19 23:53:29 -0600

    Bank

--- R/constants.R
+++ R/constants.R
@@ -1,6 +1,6 @@
 ITEM_STATUSES <- c("Available", "On Hold", "Sold", "Pawned", "Archived")
 EMPLOYEE_ROLES <- c("Owner", "Manager", "Cashier", "Appraiser", "Inventory Clerk")
-PAYMENT_METHODS <- c("Cash", "Card (simulated)", "Store credit", "Other")
+PAYMENT_METHODS <- c("Cash", "Bank", "Store credit", "Other")
 
 currency <- function(value) paste0("$", formatC(as.numeric(value), format = "f", digits = 2, big.mark = ","))
 new_number <- function(prefix) paste0(prefix, format(Sys.time(), "%Y%m%d%H%M%S"), sample(100:999, 1))
